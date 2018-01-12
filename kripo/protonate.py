e684a0eee6d24ec1a1a834f268ae04b4e13a5d12
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2018-01-12 12:14:42 +0100

    Allow for protonation of whole pdb, aminoacids only or het atoms only.

--- kripo/protonate.py
+++ kripo/protonate.py
@@ -54,7 +54,7 @@ def protonate_ligand(pdb_block: str, ph=7.4) -> str:
     return mol.write('pdb')
 
 
-def protonate(pdb: Pdb) -> Pdb:
+def protonate(pdb: Pdb, aa=True, het=True) -> Pdb:
     """Hydrogenate a first model of PDB
 
     1. Passes generic molecules to protonate_ligand,  ligands which already contain hydrogens are skipped
@@ -63,42 +63,49 @@ def protonate(pdb: Pdb) -> Pdb:
 
     Args:
         pdb: The pdb to hydrogenate
+        aa: If true then protonate protein
+        het: If true then protonate ligands
 
     Returns:
         A pdb with hydrogens added
     """
-    model = pdb.model()
-    max_serial_number = max([a.atom_id() for a in model.atoms() if a.atom_id()])
-    h_atom_id = max_serial_number + 1
+    if het:
+        model = pdb.model()
+        max_serial_number = max([a.atom_id() for a in model.atoms() if a.atom_id()])
+        h_atom_id = max_serial_number + 1
 
-    # Protonate ligands
-    ligands_model = Model()
-    [ligands_model.add_molecule(l) for l in model.molecules(generic=True) if len(l.atoms(element='H')) == 0]
-    ligands_block = ligands_model.to_file_string('pdb')
-    protonated_ligands_block = protonate_ligand(ligands_block)
-    protonated_ligands_pdb = pdb_dict_to_pdb(pdb_string_to_pdb_dict(protonated_ligands_block))
-    protonated_ligands_model = protonated_ligands_pdb.model()
+        # Protonate ligands
+        ligands_model = Model()
+        [ligands_model.add_molecule(l) for l in model.molecules(generic=True) if len(l.atoms(element='H')) == 0]
+        ligands_block = ligands_model.to_file_string('pdb')
+        protonated_ligands_block = protonate_ligand(ligands_block)
+        protonated_ligands_pdb = pdb_dict_to_pdb(pdb_string_to_pdb_dict(protonated_ligands_block))
+        protonated_ligands_model = protonated_ligands_pdb.model()
 
-    # Add hydrogens of protonated_ligands_pdb to model
-    for l in protonated_ligands_model.molecules(generic=True):
-        for a in l.atoms():
-            for ba in a.bonded_atoms():
-                if ba.element() == 'H':
-                    newh = Atom(element='H',
-                                x=ba.location()[0],
-                                y=ba.location()[1],
-                                z=ba.location()[2],
-                                atom_id=h_atom_id,
-                                name=ba.name())
-                    mol = model.molecule(name=l.name())
-                    heavy = mol.atom(name=a.name())
-                    model.add_atom(newh)
-                    mol.add_atom(newh)
-                    heavy.bond(newh)
-                    h_atom_id += 1
+        # Add hydrogens of protonated_ligands_pdb to model
+        for l in protonated_ligands_model.molecules(generic=True):
+            for a in l.atoms():
+                for ba in a.bonded_atoms():
+                    if ba.element() == 'H':
+                        newh = Atom(element='H',
+                                    x=ba.location()[0],
+                                    y=ba.location()[1],
+                                    z=ba.location()[2],
+                                    atom_id=h_atom_id,
+                                    name=ba.name())
+                        mol = model.molecule(name=l.name())
+                        heavy = mol.atom(name=a.name())
+                        model.add_atom(newh)
+                        mol.add_atom(newh)
+                        heavy.bond(newh)
+                        # newh.bond(heavy)
+                        h_atom_id += 1
 
-    # Protonate whole pdb
-    unprotonated_block = pdb.to_file_string()
-    protonated_block = protonate_protein(unprotonated_block)
+    if aa:
+        # Protonate whole pdb
+        unprotonated_block = pdb.to_file_string()
+        protonated_block = protonate_protein(unprotonated_block)
 
-    return pdb_dict_to_pdb(pdb_string_to_pdb_dict(protonated_block[0]))
+        return pdb_dict_to_pdb(pdb_string_to_pdb_dict(protonated_block[0]))
+    else:
+        return pdb
