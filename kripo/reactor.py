9b2ffaaf5f645ed863355ad012150fd54f160449
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-02-22 14:06:56 +0100

    Add unique coordinates to r groups

--- kripo/reactor.py
+++ kripo/reactor.py
@@ -2,10 +2,11 @@ import logging
 import pkg_resources
 from typing import Dict
 
-from rdkit.Chem import AllChem, MolToSmiles, SanitizeMol, Mol, Atom
+from rdkit.Chem import AllChem, MolToSmiles, SanitizeMol, Mol, Atom, MolToMolBlock
+from rdkit.Geometry.rdGeometry import Point3D
 
 
-def map_atoms(mol_a: Mol, mol_b: Mol) -> Dict[Atom, Atom]:
+def map_atoms(mol_a: Mol, mol_b: Mol) -> Dict[int, int]:
     """Find all atoms in mol_a in mol_b
 
     Args:
@@ -13,7 +14,7 @@ def map_atoms(mol_a: Mol, mol_b: Mol) -> Dict[Atom, Atom]:
         mol_b:
 
     Returns:
-        Dictionary where key are atoms from mol_a and values are atoms from mol_b
+        Dictionary where key are atom indices from mol_a and values are atom indices from mol_b
     """
     a_conf = mol_a.GetConformer()
     b_conf = mol_b.GetConformer()
@@ -24,12 +25,13 @@ def map_atoms(mol_a: Mol, mol_b: Mol) -> Dict[Atom, Atom]:
     for a in mol_a.GetAtoms():
         a_pos = a_conf.GetAtomPosition(a.GetIdx())
         for b in mol_b.GetAtoms():
-            if b in b2a_atoms:
+            if b.GetIdx() in b2a_atoms:
+                # Don't try to map same atom twice
                 continue
             bp_pos = b_conf.GetAtomPosition(b.GetIdx())
-            if b.GetSymbol() == a.GetSymbol() and a_pos.x == bp_pos.x and a_pos.y == bp_pos.y and a_pos.z == bp_pos.z:
-                a2b_atoms[a] = b
-                b2a_atoms[b] = a
+            if b.Match(a) and a_pos.x == bp_pos.x and a_pos.y == bp_pos.y and a_pos.z == bp_pos.z:
+                a2b_atoms[a.GetIdx()] = b.GetIdx()
+                b2a_atoms[b.GetIdx()] = a.GetIdx()
 
     return a2b_atoms
 
@@ -45,13 +47,13 @@ def embed_r_groups(mol: Mol, parent: Mol):
         LookupError: When atoms bonded to R group can not be found in parent
 
     """
-    mol2parent_atoms = map_atoms(mol, parent)
-    mol2parent_idxs = {k.GetIdx(): v.GetIdx() for k, v in mol2parent_atoms.items()}
+    mol2parent_idxs = map_atoms(mol, parent)
     parent2mol_idxs = {v for v in mol2parent_idxs.values()}
     idx2parent_atom = {a.GetIdx(): a for a in parent.GetAtoms()}
 
     mol_conf = mol.GetConformer()
     parent_conf = parent.GetConformer()
+    r_pos_cache = set()
     for r_atom in mol.GetAtoms():
         if r_atom.GetSymbol() == '*':
             for r_bond in r_atom.GetBonds():
@@ -62,7 +64,6 @@ def embed_r_groups(mol: Mol, parent: Mol):
                     # same atom in parent
                     r_bonded_atom_parent_idx = mol2parent_idxs[r_bonded_idx]
                     r_bonded_atom_parent = idx2parent_atom[r_bonded_atom_parent_idx]
-
                     for r_bonded_atom_parent_bond in r_bonded_atom_parent.GetBonds():
                         # Atom bonded to atom which is bonded to R group in parent
                         r_bonded_atom_parend_bonded_atom = r_bonded_atom_parent_bond.GetOtherAtom(r_bonded_atom_parent)
@@ -70,12 +71,17 @@ def embed_r_groups(mol: Mol, parent: Mol):
                         if r_bonded_atom_parend_bonded_atom_idx not in parent2mol_idxs:
                             # atom in parent but not in mol which was replaced R group
                             point = parent_conf.GetAtomPosition(r_bonded_atom_parend_bonded_atom_idx)
-                            mol_conf.SetAtomPosition(r_atom.GetIdx(), point)
-                            break
-                    else:
-                        raise LookupError('Atom bonded to atom which is bonded to R group in parent not found')
+                            if serialize_point(point) not in r_pos_cache:
+                                # Position has not been used for other R group
+                                mol_conf.SetAtomPosition(r_atom.GetIdx(), point)
+                                r_pos_cache.add(serialize_point(point))
+                                break
                 else:
                     raise LookupError('Atom bonded to R group not found in parent')
+
+
+def serialize_point(point: Point3D):
+    return str(point.x) + str(point.y) + str(point.z)
 
 
 class Reactor:
